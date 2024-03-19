d29f203f025a6f801f038c1f255a36e8328cc6ed
s24218 <s24218@pjwstk.edu.pl>
2024-03-19 16:31:20 +0100

    feature added

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -9,3 +9,13 @@ Console.WriteLine("murrr");
 
 // Third modification
 Console.WriteLine("HAHAHAHA");
+static double CountAverage(int[] Arr)
+{
+    double avg = 0;
+    int len = Arr.Length;
+    for (int i = 0; i < len; i++)
+    {
+        avg += Arr[i] / len;
+    }
+    return avg;
+}
