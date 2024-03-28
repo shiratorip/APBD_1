e706b5cc0a21f3aadab45430601d77d5d0dfc56f
s24218 <s24218@pjwstk.edu.pl>
2024-03-28 20:09:48 +0100

    initial Container implementation

---
+++ APBD2/APBD2/Program.cs
@@ -0,0 +1,8 @@
+﻿using ContainerSpace;
+
+Container container = new Container(5000, 200, 100, 150, 10000);
+Container container2 = new Container(5000, 200, 100, 150, 10000);
+Container container3 = new Container(5000, 200, 100, 150, 10000);
+Console.WriteLine(container);
+Console.WriteLine(container2);
+Console.WriteLine(container3);
